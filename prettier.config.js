5c5363a67e88ed416a8363e080b429b8d3aaa64c
wcs <1103409364@qq.com>
2022-05-05 18:18:03 +0800

    format
    Signed-off-by: wcs <1103409364@qq.com>

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/**
+ * @description 代码规范
+ */
+
+module.exports = {
+  printWidth: 105,
+  tabWidth: 2,
+  singleQuote: false,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  htmlWhitespaceSensitivity: 'ignore',
+  vueIndentScriptAndStyle: true,
+}
